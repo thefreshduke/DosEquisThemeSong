61bad2c03f8049780c2b166477bbcb6ac5b318ce
thefreshduke <scottyshaw1@gmail.com>
2016-02-06 17:12:13 +0000

    Basic layout and looping music complete; alarm and Twitter next

---
+++ XXAlarmClock/AlarmViewController.swift
@@ -0,0 +1,59 @@
+//
+//  AlarmViewController.swift
+//  XXAlarmClock
+//
+//  Created by Scotty Shaw on 2/5/16.
+//  Copyright © 2016 ___sks6___. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class AlarmViewController: UIViewController {
+    
+    var dosEquisThemeSong: AVAudioPlayer?
+    
+//    @IBOutlet weak var currentTimeDisplayLabel: UITextField!
+    
+    @IBAction func playDosEquisThemeSong(sender: AnyObject) {
+        dosEquisThemeSong?.numberOfLoops = -1
+        dosEquisThemeSong?.volume = 1
+        dosEquisThemeSong?.play()
+    }
+    
+    @IBAction func stopDosEquisThemeSong(sender: AnyObject) {
+        dosEquisThemeSong?.stop()
+        dosEquisThemeSong?.currentTime = 0
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let dosEquisThemeSong = self.setupAudioPlayerWithFile("DosEquisThemeSong", type:"mp3") {
+            self.dosEquisThemeSong = dosEquisThemeSong
+        }
+        
+        // add repeat
+    }
+    
+    func setupAudioPlayerWithFile(file: NSString, type: NSString) -> AVAudioPlayer?  {
+        
+        // 1
+        let path = NSBundle.mainBundle().pathForResource(file as String, ofType: type as String)
+        let url = NSURL.fileURLWithPath(path!)
+        
+        // 2
+        var audioPlayer: AVAudioPlayer?
+        
+        // 3
+        do {
+            try audioPlayer = AVAudioPlayer(contentsOfURL: url)
+        }
+        catch {
+            print("Player not available")
+        }
+        
+        // 4
+        return audioPlayer
+    }
+    
+}
