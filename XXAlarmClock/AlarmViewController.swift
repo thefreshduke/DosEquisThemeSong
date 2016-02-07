9d2444ffccb99d65ef02412ff931c40c83b04b2c
thefreshduke <scottyshaw1@gmail.com>
2016-02-07 20:07:32 +0800

    Harcoded version of setting alarm time implemented; need to implement sign-in status storage; need to implement Twitter sign-out; need to implement sign-in auto-check in ViewController.swift to conditionally bypass LogInViewController.swift; need to implement fill-in-the-blank Tweet form; need to improve implementation of setting alarm time

--- XXAlarmClock/AlarmViewController.swift
+++ XXAlarmClock/AlarmViewController.swift
@@ -15,7 +15,37 @@ class AlarmViewController: UIViewController {
     var dosEquisThemeSong: AVAudioPlayer?
     var timer = NSTimer()
     var startingTime = NSTimeInterval()
+    
+    @IBOutlet weak var alarmTimeLabel: UITextField!
+    
+    var alarmHour: Int = 20
+    var alarmMinute: Int = 04
+    var alarmSecond: Int = 00
+    
     @IBOutlet weak var timeDisplayLabel: UITextField!
+    
+    
+    @IBAction func setAlarmTime(sender: AnyObject) {
+//        let alert = UIAlertController(title: "Set alarm time", message: "", preferredStyle: UIAlertControllerStyle.Alert)
+//        
+//        var alarmTimeString: UITextField
+//        
+//        alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: { (action) -> Void in
+//            alarmTimeString = alert.textFields![0] as UITextField
+//            print("Text field: \(alarmTimeString.text)")
+//        }))
+
+//        alarmHour = Range<String.Index>(start: alarmTime.0, end: alarmTime.2)
+//        alarmMinute = Range(start: alarmTime.3, end: alarmTime.5)
+//        alarmSecond = Range(start: alarmTime.6, end: alarmTime.8)
+        
+//        alert.addTextFieldWithConfigurationHandler({(textField: UITextField!) in
+//            textField.placeholder = "Enter text:"
+//        })
+//        self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    
     
     @IBAction func playDosEquisThemeSong(sender: AnyObject) {
         dosEquisThemeSong?.numberOfLoops = -1
@@ -26,7 +56,7 @@ class AlarmViewController: UIViewController {
     @IBAction func stopDosEquisThemeSong(sender: AnyObject) {
         let composer = TWTRComposer()
         
-        composer.setText("just setting up my Fabric")
+        composer.setText("I don't always ... But when I do, I...")
         composer.setImage(UIImage(named: "fabric"))
         
         // Called from a UIViewController
@@ -51,6 +81,12 @@ class AlarmViewController: UIViewController {
         let aSelector : Selector = "updateTime"
         timer = NSTimer.scheduledTimerWithTimeInterval(0.01, target: self, selector: aSelector, userInfo: nil, repeats: true)
         startingTime = NSDate.timeIntervalSinceReferenceDate()
+        
+        let strAlarmHour = String(format: "%02d", alarmHour)
+        let strAlarmMinute = String(format: "%02d", alarmMinute)
+        let strAlarmSecond = String(format: "%02d", alarmSecond)
+        
+        alarmTimeLabel.text = "\(strAlarmHour):\(strAlarmMinute):\(strAlarmSecond)"
     }
     
     func setupAudioPlayerWithFile(file: NSString, type: NSString) -> AVAudioPlayer?  {
@@ -79,38 +115,11 @@ class AlarmViewController: UIViewController {
     func updateTime () {
         let date = NSDate()
         let calendar = NSCalendar.currentCalendar()
-        let components = calendar.components([.Second, .Minute, .Hour, .Day , .Month , .Year], fromDate: date)
+        let components = calendar.components([.Second, .Minute, .Hour], fromDate: date)
         
-        let year =  components.year
-        let month = components.month
-        let day = components.day
         let hour = components.hour
         let minute = components.minute
         let second = components.second
-        
-//        print(year)
-//        print(month)
-//        print(day)
-        
-//        let currentTime = NSDate.timeIntervalSinceReferenceDate()
-//        
-//        // calculate elapsed time
-//        var elapsedTime = currentTime
-//        
-//        // calculate elapsed hours
-//        let hours = UInt8(elapsedTime / 3600.0)
-//        elapsedTime -= NSTimeInterval(hours) * 3600.0
-//        
-//        // calculate elapsed minutes
-//        let minutes = UInt8(elapsedTime / 60.0)
-//        elapsedTime -= NSTimeInterval(minutes) * 60.0
-//        
-//        // calculate elapsed seconds
-//        let seconds = UInt8(elapsedTime)
-//        elapsedTime -= NSTimeInterval(seconds)
-//        
-//        // calculate elasped milliseconds
-////        let milliseconds = UInt8(elapsedTime * 100)
         
         // add the leading zero for minutes, seconds and milliseconds and store them as string constants
         let strHour = String(format: "%02d", hour)
@@ -119,5 +128,11 @@ class AlarmViewController: UIViewController {
         
         // concatenate minutes, seconds and milliseconds and assign them to the UILabel
         timeDisplayLabel.text = "\(strHour):\(strMinute):\(strSecond)"
+        
+        if (alarmHour == hour && alarmMinute == minute && alarmSecond == second) {
+            dosEquisThemeSong?.numberOfLoops = -1
+            dosEquisThemeSong?.volume = 1
+            dosEquisThemeSong?.play()
+        }
     }
 }
