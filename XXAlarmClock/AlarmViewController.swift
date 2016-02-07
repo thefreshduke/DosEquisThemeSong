4c5c90546744400c40f6e0c7f9e9eedd4ab4f668
thefreshduke <scottyshaw1@gmail.com>
2016-02-07 19:31:55 +0800

    Implemented 24-hour block and basic time display based on local time of device

--- XXAlarmClock/AlarmViewController.swift
+++ XXAlarmClock/AlarmViewController.swift
@@ -13,6 +13,9 @@ import TwitterKit
 class AlarmViewController: UIViewController {
     
     var dosEquisThemeSong: AVAudioPlayer?
+    var timer = NSTimer()
+    var startingTime = NSTimeInterval()
+    @IBOutlet weak var timeDisplayLabel: UITextField!
     
     @IBAction func playDosEquisThemeSong(sender: AnyObject) {
         dosEquisThemeSong?.numberOfLoops = -1
@@ -37,7 +40,6 @@ class AlarmViewController: UIViewController {
                 self.dosEquisThemeSong?.currentTime = 0
             }
         }
-
     }
     
     override func viewDidLoad() {
@@ -45,6 +47,10 @@ class AlarmViewController: UIViewController {
         if let dosEquisThemeSong = self.setupAudioPlayerWithFile("DosEquisThemeSong", type:"mp3") {
             self.dosEquisThemeSong = dosEquisThemeSong
         }
+        
+        let aSelector : Selector = "updateTime"
+        timer = NSTimer.scheduledTimerWithTimeInterval(0.01, target: self, selector: aSelector, userInfo: nil, repeats: true)
+        startingTime = NSDate.timeIntervalSinceReferenceDate()
     }
     
     func setupAudioPlayerWithFile(file: NSString, type: NSString) -> AVAudioPlayer?  {
@@ -70,4 +76,48 @@ class AlarmViewController: UIViewController {
         return audioPlayer
     }
     
+    func updateTime () {
+        let date = NSDate()
+        let calendar = NSCalendar.currentCalendar()
+        let components = calendar.components([.Second, .Minute, .Hour, .Day , .Month , .Year], fromDate: date)
+        
+        let year =  components.year
+        let month = components.month
+        let day = components.day
+        let hour = components.hour
+        let minute = components.minute
+        let second = components.second
+        
+//        print(year)
+//        print(month)
+//        print(day)
+        
+//        let currentTime = NSDate.timeIntervalSinceReferenceDate()
+//        
+//        // calculate elapsed time
+//        var elapsedTime = currentTime
+//        
+//        // calculate elapsed hours
+//        let hours = UInt8(elapsedTime / 3600.0)
+//        elapsedTime -= NSTimeInterval(hours) * 3600.0
+//        
+//        // calculate elapsed minutes
+//        let minutes = UInt8(elapsedTime / 60.0)
+//        elapsedTime -= NSTimeInterval(minutes) * 60.0
+//        
+//        // calculate elapsed seconds
+//        let seconds = UInt8(elapsedTime)
+//        elapsedTime -= NSTimeInterval(seconds)
+//        
+//        // calculate elasped milliseconds
+////        let milliseconds = UInt8(elapsedTime * 100)
+        
+        // add the leading zero for minutes, seconds and milliseconds and store them as string constants
+        let strHour = String(format: "%02d", hour)
+        let strMinute = String(format: "%02d", minute)
+        let strSecond = String(format: "%02d", second)
+        
+        // concatenate minutes, seconds and milliseconds and assign them to the UILabel
+        timeDisplayLabel.text = "\(strHour):\(strMinute):\(strSecond)"
+    }
 }
